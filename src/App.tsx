ca411b90fb2cac2d666552b00088936ce38be089
yijiahe <yi.jiahe00@gmail.com>
2023-12-02 06:56:21 +0000

    Add day 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,58 @@
 import React from 'react';
 import './App.css';
 import { useState } from 'react';
-import init, { day_1_get_sum_of_calibration_values_in_document, day_1_get_sum_of_calibration_values_in_document_part_2 } from "aoc-restore-snow-operations";
+import init, {
+  day_1_get_sum_of_calibration_values_in_document, day_1_get_sum_of_calibration_values_in_document_part_2,
+  day_2_get_sum_of_possible_game_ids, day_2_get_sum_of_minimum_power
+} from "aoc-restore-snow-operations";
 
 function App() {
   const [answer, setAnswer] = useState("");
 
-  const raiseError = () => {
-    alert(`Unable to generate solution.
+  const generateAnswer = (f: (input: string) => any) => {
+    init().then(() => {
+      try {
+        const puzzle_input = (document.getElementById('puzzle-input')! as HTMLTextAreaElement).value;
+        setAnswer(f(puzzle_input).toString());
+      } catch {
+        alert(`Unable to generate solution.
 
-    Please ensure your input is valid. 
-                    
-    If the problem persists, please submit a bug report with the day and part.`);
+        Please ensure your input is valid. 
+                        
+        If the problem persists, please submit a bug report with the day and part.`);
+      }
+    });
   };
+
+  const solutions = [
+    [day_1_get_sum_of_calibration_values_in_document, day_1_get_sum_of_calibration_values_in_document_part_2],
+    [day_2_get_sum_of_possible_game_ids, day_2_get_sum_of_minimum_power],
+  ];
 
   return (
     <div className="App">
-      <header className="App-header">
       <h1>Puzzle Input</h1>
       <textarea id="puzzle-input" />
-        <h1>2023</h1>
-        <h2>Day 1</h2>
-        <button
-          onClick={() => {
-            init().then(() => {
-              try {
-                const puzzle_input = (document.getElementById('puzzle-input')! as HTMLTextAreaElement).value;
-                setAnswer(day_1_get_sum_of_calibration_values_in_document(puzzle_input).toString());
-              } catch {
-                raiseError();
-              }
-            });
-          }}>Part 1</button>
-                  <button
-          onClick={() => {
-            init().then(() => {
-              try {
-                const puzzle_input = (document.getElementById('puzzle-input')! as HTMLTextAreaElement).value;
-                setAnswer(day_1_get_sum_of_calibration_values_in_document_part_2(puzzle_input).toString());
-              } catch {
-                raiseError();
-              }
-            });
-          }}>Part 2</button>
-          <h1>Answer</h1>
-        <p>{answer}</p>
-      </header>
+      <h1>Answer</h1>
+      <p>{answer}</p>
+      <h1>2023</h1>
+      <div className='Calendar-Grid'>
+        {Array.from(Array(25)).map((_, i) => {
+          return (
+            <div key={i}>
+              <p>Day {i + 1}</p>
+              {Array.from(Array(2)).map((_, j) => {
+                try {
+                  const f = solutions[i][j];
+                  return <button onClick={() => { generateAnswer(f) }} key={j}>Part {j+1}</button>
+                } catch {
+                  return <button disabled key={j}>Part {j+1}</button>
+                }
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
